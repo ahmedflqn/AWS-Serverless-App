20fbc3000775594dd4f2f828a7f3d1b2324a9a7e
“Your <ahmedflqn98@gmail.com>
2023-10-19 03:55:39 +0200

    added pool, client id to config.js

---
+++ website_files/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Bwq7H6YuO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 't1agbj7smp18lnkqiurdfac8v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
